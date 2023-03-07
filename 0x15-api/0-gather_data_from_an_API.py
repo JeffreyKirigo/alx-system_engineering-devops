59976b5110be00846b786944952b5d344b1487e0
JeffreyKirigo <jeffreyem99@gmail.com>
2023-03-07 09:24:05 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    num = sys.argv
+    if len(num) > 1:
+        url = 'https://jsonplaceholder.typicode.com'
+        user = requests.get(f'{url}/users/{num[1]}').json()
+        todos = requests.get(f'{url}/todos', params={"userId": num[1]}).json()
+
+        completed = [t.get("title") for t in todos if t.get("completed") is True]
+        print(f'Employee {user.get("name")} is done with tasks({len(completed)}/{len(todos)})')
+        for c in completed:
+            print(f'\t{c}')
