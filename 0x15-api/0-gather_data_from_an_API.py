69bbd6e94016f7a7f0f4fefd7d27a2ab108e0ef1
JeffreyKirigo <jeffreyem99@gmail.com>
2023-03-07 10:20:51 +0300

    Get data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,11 +6,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    num = sys.argv
+    if len(num) > 1:
+        url = 'https://jsonplaceholder.typicode.com'
+        user = requests.get(f'{url}/users/{num[1]}').json()
+        todos = requests.get(f'{url}/todos', params={"userId": num[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        completed = [t.get("title")
+                     for t in todos if t.get("completed") is True]
+        print(
+            f'Employee {user.get("name")} is done with tasks({len(completed)}/{len(todos)})')
+        for c in completed:
+            print(f'\t{c}')
